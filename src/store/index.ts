c6e306d37634289e03c918aa20de3425e2156542
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-11-05 13:31:17 -0300

    Getting all properties in poll response: questions and alternatives

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,10 +3,14 @@ import { createStore } from 'vuex'
 import ApiResponse from '@/entities/ApiResponse';
 import Poll from '@/entities/Poll';
 import Question from '@/entities/Question';
+import Alternative from '@/entities/Alternative';
 
 export default createStore({
 
-  // 
+  /**
+   * 
+   * 
+  */
   state: {
 
     // Data's System - Poll (All Polls from the server)
@@ -21,7 +25,10 @@ export default createStore({
   },
 
 
-
+  /** 
+   * 
+   * 
+  */
   getters: {
     getAllPolls(state) : Array<Poll>{
       return state.polls
@@ -32,21 +39,58 @@ export default createStore({
 
   mutations: {
 
-    // Method for including all Polls in polls state
+    /** 
+     * Method for including all Polls in polls state
+     * 
+    */ 
     loadPolls(state, response : ApiResponse){
       if(response.statusCode === 200){
+        
+        // For each Poll
         response.content.forEach((resp, key) => {
+          console.log(resp)
+
+          // For each question in Poll
+          const questions = resp.questions
+          const computedQuestions = [] as Array<any>
+          questions.forEach((question: any) => {
+            
+            // Constructing alternatives for each question
+            const alternatives = question.alternatives
+            const computedAlternatives = [] as Array<any>
+            alternatives.forEach((alternative: any) => {
+              // Instance of Alternative
+              const computedAlternative = new Alternative(
+                alternative.value,
+                alternative.description
+              );
+              computedAlternatives.push(computedAlternative)
+            })
+
+            const computedQuestion = new Question(
+              question.order_question,
+              question.statement,
+              question.has_comment,
+              computedAlternatives
+            );
+            computedQuestions.push(computedQuestion)
+          });
           // Creating poll object based in Poll entity
+          
           const poll = new Poll(
               resp.id,
               resp.title, 
               resp.description, 
               resp.begin_at, 
-              resp.finish_at
+              resp.finish_at,
+              computedQuestions
             );
 
           // Pushing into state  
           state.polls.push(poll)
+
+          console.log(poll)
+
         })
       }
     },
