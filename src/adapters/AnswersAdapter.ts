f79a50d5f8123ff45aaf8fac15a0f5c55b0b21ec
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-11-28 21:48:43 -0300

    Creating adapter for API request

--- src/adapters/AnswersAdapter.ts
+++ src/adapters/AnswersAdapter.ts
@@ -1,3 +1,27 @@
+import Answer from "@/entities/Answer";
+import IAnswer from "@/interfaces/IAnswer";
+
+/**
+ * 
+ * 
+*/
 export class AnswerAdapter {
     
+    protected answers : Array<Answer>
+    protected idUser  : number
+    
+    constructor(
+        answers : Array<Answer>,
+        idUser  : number
+    ){
+        this.answers = answers
+        this.idUser = idUser
+    }
+
+    /**
+     * 
+    */
+    adapt(){
+
+    }
 }
